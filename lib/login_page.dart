4b9fcb6c82ba6fcc425e4a46d3393515e45b1215
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2023-01-02 04:45:26 -0300

    first commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,62 +12,110 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                Container(height: 20),
-                TextField(
-                  onChanged:((value) {
-                    email = value;
-                  }),
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(
+                      'assets/images/logo.png', 
+                      color: Colors.white
+                    ),
                   ),
-                ),
-                const SizedBox(height: 15),
-                TextField(
-                  onChanged:((value) {
-                    password = value;
-                  }),
-                  
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  Container(height: 20),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 12,
+                        right: 12,
+                        top: 16,
+                        bottom: 16,
+                      ),
+                      child: Column(
+                        children: [
+                          TextField(
+                            onChanged:((value) {
+                              email = value;
+                            }),
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                          TextField(
+                            onChanged:((value) {
+                              password = value;
+                            }),
+                            
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                          ElevatedButton(
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.white, backgroundColor: Colors.black,
+                            ),
+                            onPressed: (() { 
+                              if (email == 'teste@email.com' && password == '123') {
+                                Navigator.of(context).pushNamed('/home');
+                              } else {
+                                print('invalid');
+                              }
+                            }),
+                            child: const SizedBox(
+                              width: double.infinity,
+                              child: Text(
+                                'Entrar',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: (() { 
-                    if (email == 'teste@email.com' && password == '123') {
-                      Navigator.of(context).pushNamed('/home');
-                    } else {
-                      print('invalid');
-                    }
-                  }), 
-                  child: const Text('Entrar'),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
+      ],
+    ); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/bg.jpg', 
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.3),
+          ),
+          _body(),
+        ],
       ),
     );
   }
